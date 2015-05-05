6645621b19cc8b2ea019573bfa2a24604a6571ad
Will M <william.munn@7digital.com>
2015-05-05 17:51:56 +0100

    Add comment hilighting issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,4 @@
-require.config(/*I need to pass in what's in requirejs.config.json here */);
+//require.config throws when using UMD mode as require.config is not a function
+
 require(['wibbble'], function (wibble) { console.log(wibble); });
+//wibble is not found (the alias doesn't work)
